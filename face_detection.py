2423982f60947b715866e6d6f81f78aab31da5f1
Mahesh Shriram <98745824+mahesh1112@users.noreply.github.com>
2022-11-05 10:02:34 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -4,4 +4,11 @@ import face_recognition
 
 #for images
 
+img = cv.imread("rdj.jpg")
+face_coordinates = face_recognization.face_locations[0]
+(a,b,c,d) = face_coordinates
 
+cv.rectangle(img, (d,a), (b,c), (0,255,0), thickness = 2)
+
+cv.imshow("window_name", img)
+cv.waitKey(0)
